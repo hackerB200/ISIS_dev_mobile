e4f1fe9382671bd6ca7018db4f8349d37ccec0f5
hackerB200 <atubiaeva@gmail.com>
2024-11-28 09:09:56 +0100

    "modele ok"

--- app/src/main/java/com/example/pouet/models.kt
+++ app/src/main/java/com/example/pouet/models.kt
@@ -1,5 +1,33 @@
 package com.example.pouet
 
+data class Playlist(
+    val checksum: String,
+    val collaborative: Boolean,
+    val cover: String,
+    val creation_date: String,
+    val creator: Creator,
+    val description: String,
+    val duration: Int,
+    val fans: Int,
+    val id: Int,
+    val is_loved_track: Boolean,
+    val link: String,
+    val md5_image: String,
+    val nb_tracks: Int,
+    val picture_type: String,
+    val `public`: Boolean,
+    val share: String,
+    val title: String,
+    val tracklist: String
+)
+
+data class Creator(
+    val id: Int,
+    val name: String,
+    val tracklist: String,
+    val type: String
+)
+
 class Actor(
     var biography: String = "",
     var birthday: String = "",
